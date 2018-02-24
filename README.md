9ddcbb406405d50ea18e2a51a7c3861870347a4e
Guillaume Martigny <guillaume.martigny@gmail.com>
2018-02-24 20:45:07 +0100

    my config v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # eslint-config
-My personnal config for linting.
+My personal config for linting.
+
+Based on [Airbnb JavaScript Style Guide](https://github.com/airbnb/javascript) with a few modifications.
+
+Theses rules are my personal choices and not meant to be enforce on anyone but myself.
+If you want to make modifications, feel free to fork this repo.
---
+++ index.js
@@ -0,0 +1,35 @@
+const off = 0;
+const warn = 1;
+const error = 2;
+
+module.exports = {
+    extends: "airbnb-base",
+    env: {
+        "browser": true
+    },
+    rules: {
+        "require-jsdoc": [error, {
+            require: {
+                FunctionDeclaration: true,
+                MethodDefinition: true,
+                ClassDeclaration: true
+            }
+        }],
+        "no-param-reassign": [error, {
+            props: false
+        }],
+        "no-return-assign": off,
+        "max-len": [error, {
+            code: 120
+        }],
+        "no-plusplus": off,
+        "brace-style": [error, "stroustrup"],
+        "quotes": [error, "double"],
+        "valid-jsdoc": [error, {
+            requireReturn: false,
+            requireReturnDescription: false
+        }],
+        "indent": [error, 4],
+        "space-before-function-paren": [error, "always"]
+    }
+};
